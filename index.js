00e372d5851a506e6c6efce4274bfbadbc217541
Anil Gurung <anil_grg@hotmail.com>
2020-02-17 10:54:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+const var1 = 2;
+const var2 = 31;
+const multiply = (var1 * var2);
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 10 % 6;
+
+const max = Math.max(2,3,4,5,6,7,20);
